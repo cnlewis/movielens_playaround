9b370b1952628320d3b3bde5e3e454bf5d470b7e
cnlewis <lewiscn0@gmail.com>
2017-01-30 22:16:57 +0100

    Delete dataframes_movieslens.py

--- dataframes_movieslens.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
-users = pd.read_table('./ml-1m/users.dat', sep='::', header=None, names=unames, engine='python')
-rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
-ratings = pd.read_table('./ml-1m/ratings.dat', sep='::', header=None, names=rnames,  engine='python')
-mnames = ['movie_id', 'title', 'genres']
-movies = pd.read_table('./ml-1m/movies.dat', sep='::', header=None, names=mnames,  engine='python')
-data = pd.merge(pd.merge(ratings, users), movies)
